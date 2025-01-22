45363c023fb2f275b010fa33068e800ce9ed1810
zaurrs <zaurrs-af106@code.edu.az>
2025-01-22 19:35:48 +0400

    created

---
+++ Backend/Config/config.js
@@ -0,0 +1,13 @@
+import { connect } from "mongoose";
+
+
+
+
+
+connect("mongodb+srv://faganioaf206:Feqan20050802@classwork.3pcbv.mongodb.net/")
+.then(()=>{
+
+
+    console.log("Connected");
+    
+})
---
+++ Frontend/src/main.jsx
@@ -0,0 +1,15 @@
+
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import FavoritesPrivoder from './Context/FavoritesContext.jsx'
+import BasketPrivoder from './Context/BasketContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+<FavoritesPrivoder>
+<BasketPrivoder> 
+   <App />
+</BasketPrivoder>
+</FavoritesPrivoder>
+  
+
+)
